dc1008fe694e72f07393c98e9913ddd93768c0ea
Roberto Cannella <robertocannella@gmail.com>
2022-03-19 23:45:25 +0000

    added post method

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 const currentDate = new Date();
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 var tempReadings = [
     {
@@ -33,24 +38,33 @@ var tempReadings = [
         }]
     }]
 
-
-
-const app = express()
-
 app.get('/', (request, response) => {
     response.send(`hello world!!`);
 });
-app.get('/tempReadings', (request, response) => {
+app.get('/api/tempReadings', (request, response) => {
+    console.log('eat')
     response.send(tempReadings);
 });
-app.get('/tempReadings/:id', (request, response) => {
-    const sensor = tempReadings.find(t => t.id === parseInt(request.params.id))
-    if (!sensor) //404
+app.get('/api/tempReadings/:id', (request, response) => {
+    const sensorReading = tempReadings.find(t => t.id === parseInt(request.params.id))
+    if (!sensorReading) //404
         response.status(404).send('The sensor with the given ID was not found')
-    response.send(sensor);
+    response.send(sensorReading);
 });
 
 
+app.post('/api/tempReadings', (request, response) => {
+    console.log('RequestBody', request.body)
+    const currentId = tempReadings.find(t => t.id === parseInt(request.body.id));
+    const sensorReading = {
+        timeStamp: new Date(),
+        temperatureF: request.body.temperatureF
+    }
+    currentId.data.push(sensorReading);
+    response.send(sensorReading)
+    console.log(tempReadings)
+})
+app.use(express.json); //Middleware
 const port = process.env.PORT || 3200
 app.listen(port, () => {
     console.log(`listening on port ${port}..`)
