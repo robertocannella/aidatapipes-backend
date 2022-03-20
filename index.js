26bd92579e05e350c6a7824f7b77d47527c34617
Roberto Cannella <robertocannella@gmail.com>
2022-03-20 14:58:12 +0000

    refactored

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-const schema = Joi.object({
+const sensorSchema = Joi.object({
     id: Joi.number().integer(0).min(0).required(),
+    temperatureF: Joi.number().required()
+})
+const temperatureSchema = Joi.object({
+    timeStamp: Joi.number().integer(0).min(0).required(),
     temperatureF: Joi.number().required()
 })
 var tempReadings = [
@@ -53,22 +57,18 @@ app.get('/api/tempReadings', (request, response) => {
 // Get all temperature readings at given sensor
 app.get('/api/tempReadings/:id', (request, response) => {
     const sensorReading = tempReadings.find(t => t.id === parseInt(request.params.id))
-    if (!sensorReading)
-        response.status(404).send('A sensor with the given ID was not found')
+    if (!sensorReading) return response.status(404).send('A sensor with the given ID was not found')
 
     response.send(sensorReading);
 });
 
 // Adds a temperature reading at a given sensor id
 app.post('/api/tempReadings', (request, response) => {
-    const result = schema.validate(request.body)
-    if (result.error)
-        response.status(400).send(result.error.message)
-
+    const { error } = sensorSchema.validate(request.body)
+    if (error) return response.status(400).send(result.error.message);
 
     const currentId = tempReadings.find(t => t.id === parseInt(request.body.id));
-    if (!currentId)
-        response.status(404).send('A sensor with the given ID was not found')
+    if (!currentId) return response.status(404).send('A sensor with the given ID was not found')
 
     const sensorReading = {
         timeStamp: new Date().getTime(),
@@ -76,46 +76,38 @@ app.post('/api/tempReadings', (request, response) => {
     }
     currentId.data.push(sensorReading);
     response.send(sensorReading)
-
 })
 
-// UPDATE A Temperature Reading at a given sensor ID
+// UPDATE a Temperature Reading at a given sensor ID
 app.put('/api/tempReadings/:id', (request, response) => {
     const sensorReading = tempReadings.find(t => t.id === parseInt(request.params.id));
+    if (!sensorReading) return response.status(404).send('A sensor with the given ID was not found');
 
-    if (!sensorReading) {
-        response.status(404).send('A sensor with the given ID was not found')
-        return;
-    }
-    const result = schema.validate(request.body)
-    if (result.error)
-        response.status(400).send(result.error.message)
+    const { error, result } = temperatureSchema.validate(request.body)
+    if (error) return response.status(400).send(error.message)
 
     const timeSeries = sensorReading.data.find(data => data.timeStamp === request.body.timeStamp)
-    timeSeries.temperatureF = request.body.temperatureF
+    timeSeries.temperatureF = request.body.temperatureF;
     response.send(tempReadings)
 })
 
-// deletes all sensor data from a given sensor
+// DELETE all sensor data at a given sensor
 app.delete('/api/tempReadings/:id', (request, response) => {
     const currentSensor = tempReadings.find(t => t.id === parseInt(request.params.id));
-    if (!currentSensor)
-        response.status(404).send('A sensor with the given ID was not found')
+    if (!currentSensor) return response.status(404).send('A sensor with the given ID was not found');
 
     const index = tempReadings.indexOf(currentSensor)
     tempReadings.splice(index, 1);
 
     response.send(currentSensor)
 })
-// Deletes a temperature reading at given sensor, at a given timestamp
+// DELETE a temperature reading from given sensor, at a given timestamp
 app.delete('/api/tempReadings/:id/:timeStamp', (request, response) => {
     const currentSensor = tempReadings.find(t => t.id === parseInt(request.params.id));
     const currentTimeSeries = currentSensor.data.find(t => t.timeStamp === parseInt(request.params.timeStamp));
 
-    if (!currentSensor)
-        response.status(404).send('A Sensor with the given ID was not found')
-    if (!currentTimeSeries)
-        response.status(404).send(`A time stamp ${request.params.timeStamp} at Sensor ${currentSensor.id} was not found`)
+    if (!currentSensor) return response.status(404).send('A Sensor with the given ID was not found');
+    if (!currentTimeSeries) return response.status(404).send(`A time stamp ${request.params.timeStamp} at Sensor ${currentSensor.id} was not found`)
 
     const sensorIndex = tempReadings.indexOf(currentSensor);
     const timeStampIndex = tempReadings[sensorIndex].data.indexOf(currentTimeSeries);
