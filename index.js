63d7fe78c3c690b3d692c0a7d7481b5a1a984423
Roberto Cannella <robertocannella@gmail.com>
2022-03-20 04:02:10 +0000

    added put

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import Joi from 'joi'
 
+
+
 const currentDate = new Date();
 const app = express()
 app.use(express.urlencoded({ extended: true }))
@@ -18,7 +20,7 @@ var tempReadings = [
             temperatureF: 84.88
         },
         {
-            timeStamp: currentDate.getTime() + 15 * 60000,
+            timeStamp: 1647748484997,
             temperatureF: 86.89
         }]
     },
@@ -33,7 +35,7 @@ var tempReadings = [
             temperatureF: 34.88
         },
         {
-            timeStamp: currentDate.getTime() + 15 * 60000,
+            timeStamp: 1647748484997,
             temperatureF: 45.89
         }]
     }]
@@ -71,11 +73,34 @@ app.post('/api/tempReadings', (request, response) => {
         response.status(404).send('A sensor with the given ID was not found')
 
     const sensorReading = {
-        timeStamp: new Date(),
+        timeStamp: new Date().getTime(),
         temperatureF: request.body.temperatureF
     }
     currentId.data.push(sensorReading);
     response.send(sensorReading)
+
+})
+
+app.put('/api/tempReadings/:id', (request, response) => {
+    const schema = Joi.object({
+        timeStamp: Joi.date().timestamp().required(),
+        temperatureF: Joi.number().required()
+    })
+    const sensorReading = tempReadings.find(t => t.id === parseInt(request.params.id));
+
+    if (!sensorReading) {
+        response.status(404).send('A sensor with the given ID was not found')
+        return;
+    }
+    const result = schema.validate(request.body)
+    if (result.error)
+        response.status(400).send(result.error.message)
+
+
+    const timeSeries = sensorReading.data.find(data => data.timeStamp === request.body.timeStamp)
+    timeSeries.temperatureF = request.body.temperatureF
+    response.send(tempReadings)
+    console.log(tempReadings)
 })
 
 const port = process.env.PORT || 3200
