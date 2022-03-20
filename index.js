116c36e649780a094e93e168f00e431a114aac05
Roberto Cannella <robertocannella@gmail.com>
2022-03-20 14:40:07 +0000

    added delete operations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { response } from 'express';
 import Joi from 'joi'
 
 
@@ -8,6 +9,10 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+const schema = Joi.object({
+    id: Joi.number().integer(0).min(0).required(),
+    temperatureF: Joi.number().required()
+})
 var tempReadings = [
     {
         id: 19283,
@@ -40,29 +45,22 @@ var tempReadings = [
         }]
     }]
 
-app.get('/', (request, response) => {
-    response.send(`hello world!!`);
-});
+// Gets all sensors and their respective data
 app.get('/api/tempReadings', (request, response) => {
     response.send(tempReadings);
 });
 
+// Get all temperature readings at given sensor
 app.get('/api/tempReadings/:id', (request, response) => {
     const sensorReading = tempReadings.find(t => t.id === parseInt(request.params.id))
-    if (!sensorReading) {
+    if (!sensorReading)
         response.status(404).send('A sensor with the given ID was not found')
-        return;
-    }
 
     response.send(sensorReading);
 });
 
-
+// Adds a temperature reading at a given sensor id
 app.post('/api/tempReadings', (request, response) => {
-    const schema = Joi.object({
-        id: Joi.number().integer(0).min(0).required(),
-        temperatureF: Joi.number().required()
-    })
     const result = schema.validate(request.body)
     if (result.error)
         response.status(400).send(result.error.message)
@@ -81,11 +79,8 @@ app.post('/api/tempReadings', (request, response) => {
 
 })
 
+// UPDATE A Temperature Reading at a given sensor ID
 app.put('/api/tempReadings/:id', (request, response) => {
-    const schema = Joi.object({
-        timeStamp: Joi.date().timestamp().required(),
-        temperatureF: Joi.number().required()
-    })
     const sensorReading = tempReadings.find(t => t.id === parseInt(request.params.id));
 
     if (!sensorReading) {
@@ -96,11 +91,37 @@ app.put('/api/tempReadings/:id', (request, response) => {
     if (result.error)
         response.status(400).send(result.error.message)
 
-
     const timeSeries = sensorReading.data.find(data => data.timeStamp === request.body.timeStamp)
     timeSeries.temperatureF = request.body.temperatureF
     response.send(tempReadings)
-    console.log(tempReadings)
+})
+
+// deletes all sensor data from a given sensor
+app.delete('/api/tempReadings/:id', (request, response) => {
+    const currentSensor = tempReadings.find(t => t.id === parseInt(request.params.id));
+    if (!currentSensor)
+        response.status(404).send('A sensor with the given ID was not found')
+
+    const index = tempReadings.indexOf(currentSensor)
+    tempReadings.splice(index, 1);
+
+    response.send(currentSensor)
+})
+// Deletes a temperature reading at given sensor, at a given timestamp
+app.delete('/api/tempReadings/:id/:timeStamp', (request, response) => {
+    const currentSensor = tempReadings.find(t => t.id === parseInt(request.params.id));
+    const currentTimeSeries = currentSensor.data.find(t => t.timeStamp === parseInt(request.params.timeStamp));
+
+    if (!currentSensor)
+        response.status(404).send('A Sensor with the given ID was not found')
+    if (!currentTimeSeries)
+        response.status(404).send(`A time stamp ${request.params.timeStamp} at Sensor ${currentSensor.id} was not found`)
+
+    const sensorIndex = tempReadings.indexOf(currentSensor);
+    const timeStampIndex = tempReadings[sensorIndex].data.indexOf(currentTimeSeries);
+
+    tempReadings[sensorIndex].data.splice(timeStampIndex, 1);
+    response.send(currentTimeSeries);
 })
 
 const port = process.env.PORT || 3200
